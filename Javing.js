081855ab50e12e8302bc16aaf6ceb64b044e7034
Robertnando <rlopez35@ucol.mx>
2023-11-17 09:25:11 -0600

    Verison con eventos

--- Javing.js
+++ Javing.js
@@ -26,6 +26,56 @@ function division() {
     document.getElementById("ElResultado").innerHTML = x/y;
 }
 
+//MISC
+const hola = document.querySelector("#MR_BEAST");
+  console.log(hola);
+
+hola.addEventListener("click", function(){
+    console.log("Clicked on MR BEAST");
+    document.getElementById("MR_BEAST").style.backgroundColor = "blue";
+    document.getElementById("MR_BEAST").style.Color = "black";
+    document.getElementById("MR_BEAST").style.boxShadow = "10px -10px 10px gray";
+    debugger;
+});
+
+hola.addEventListener("mouseout", function(){
+    console.log("May your L´s be many and your bitches few");
+});
+
+hola.addEventListener("mouseover", function(){
+    console.log("Clck it or no balls");
+});
+
+window.addEventListener("keydown", (e) =>{
+    console.log("STOOP");
+    console.log(string.fromCharCode(e.keyCode));
+});
+
+window.addEventListener("keypress", () =>{
+    console.log("OBAMA");
+});
+
+window.addEventListener("keyup", (e) =>{
+    console.log("you stopped yipee");
+});
+
+
+window.addEventListener("load", () =>{
+    console.log("ELEPHANTMARIO")
+});
+
+//TAREA
+
+let hello = document.querySelector("#TURURURU");
+hello.addEventListener("click", function(){
+    let text= document.querySelector("#PAIN");
+    document.getElementById("TURURURU").innerHTML = text;
+    debugger;
+});
+/*
+querySelector= selects class of a html object
+getElementById= selects an ID of an html object
+*/ 
 
 //Calculadora mas avanzada
 const Screen = document.querySelector(".Screen");
@@ -38,7 +88,7 @@ Button.forEach(Botton => {
         if (Botton.id === "clear") {
             Screen.textContent = "0";
         }
-        Screen.textContent += Smash;
+        Screen.textContent = Smash;
 
         console.log(Botton.textContent);
     })
